5193b93299980636fe5ba0f4cfc11751f9269bdb
Jiang Junyu <double.chiang@gmail.com>
2018-08-07 12:22:34 -0700

    fixes #6, load config file from application path.

--- globalvars.py
+++ globalvars.py
@@ -6,21 +6,33 @@ import configparser
 TOKEN_USERNAME="username"
 TOKEN_PASSWORD="password"
 TOKEN_HOST="host"
-CONFIG_FILE=".qsmcli.ses"
 
 class GlobalVars:
 
     username=""
     password=""
     host=""
+    CONFIG_FILE=".qsmcli.ses"
+    TOKEN_USERNAME="username"
+    TOKEN_PASSWORD="password"
+    TOKEN_HOST="host"
 
     @staticmethod
-    def save(cfg_filename=CONFIG_FILE):
+    def getAppConfigFile():
+        dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+        config_file = dir + '/' + GlobalVars.CONFIG_FILE
+        return config_file
+
+
+    @staticmethod
+    def save(cfg_filename=None):
+        if (cfg_filename is None):
+            cfg_filename=GlobalVars.getAppConfigFile()
         config = configparser.ConfigParser()
         config.add_section('global')
-        config.set('global', TOKEN_USERNAME, GlobalVars.username)
-        config.set('global', TOKEN_PASSWORD, GlobalVars.password)
-        config.set('global', TOKEN_HOST, GlobalVars.host)
+        config.set('global', GlobalVars.TOKEN_USERNAME, GlobalVars.username)
+        config.set('global', GlobalVars.TOKEN_PASSWORD, GlobalVars.password)
+        config.set('global', GlobalVars.TOKEN_HOST, GlobalVars.host)
 
         with open(cfg_filename, 'w') as cfgfile:
             config.write(cfgfile)
@@ -29,13 +41,18 @@ class GlobalVars:
 #        print("cfg file saved")
 
     @staticmethod
-    def load(cfg_filename=CONFIG_FILE):
+    def load(cfg_filename=None):
+        """ Load the configuration file from application path
+        """
+        if (cfg_filename is None):
+            cfg_filename=GlobalVars.getAppConfigFile()
+
         try:
             config =  configparser.ConfigParser()
             config.read(cfg_filename)
-            GlobalVars.username= config.get('global', TOKEN_USERNAME)
-            GlobalVars.password= config.get('global', TOKEN_PASSWORD)
-            GlobalVars.host= config.get('global', TOKEN_HOST)
+            GlobalVars.username= config.get('global', GlobalVars.TOKEN_USERNAME)
+            GlobalVars.password= config.get('global', GlobalVars.TOKEN_PASSWORD)
+            GlobalVars.host= config.get('global', GlobalVars.TOKEN_HOST)
 #            print("Loading configuration.....")
             config.close()
         except:
