bc36583dc5c598f5ed1f3414127cd659cad37b7d
Jiang Junyu <double.chiang@gmail.com>
2018-08-08 11:44:06 -0700

    fixes #7, support installation in windows.

--- install.py
+++ install.py
@@ -6,10 +6,13 @@ from globalvars import GlobalVars
 from exec import Exec
 
 class Install():
+
     """ Install this application into OS's path library
         In Linux, create the symbloic link in the /usr/local/bin
         In Windows, append application folder into system path variable.
     """
+
+    WINBAT='qsmcli.bat'
 
     def install_in_linux(self, arg):
         """ Create the symbolic name in /usr/local/bin directory
@@ -25,8 +28,37 @@ class Install():
         print(f"Creating symbolic link {dest} to {src}")
         os.symlink(src, dest)
 
-    def install_in_win(self):
-        print("Windows Installation not implement yet.")
+    def install_in_win(self, arg):
+        """ create a bat file in the window32 folder and start this application
+        """
+
+        # We would like to put the file in system32 folderself.
+        # However, the windows will redirect the system32 directory call
+        # so use the sysnative folder actuall point to system32 folder
+        sys32dir = os.environ['WINDIR'] + '\\sysnative\\'
+        winbat_file = sys32dir + Install.WINBAT
+
+        try:
+            # Check if file pre-exist and file permission
+            if os.path.isfile(winbat_file):
+                print(f"Destination file {winbat_file} exist, overwriting it.")
+                os.unlink(winbat_file)
+                if os.path.isfile(winbat_file):
+                    print("Error! file can't be deleted")
+                    return
+
+            # Create the batch file
+            pgm = os.path.abspath(sys.argv[0])
+            with open(winbat_file, 'w') as bat_file:
+                print(f"creating {winbat_file}")
+                bat_file.write(f"start \"\" \"{pgm}\" %*")
+                bat_file.close()
+
+        except IOError:
+            print("Write file error, run program as administrator")
+            return
+        except:
+            print("Please return error to author for your money back")
 
 
     def __init__(self, arg):
