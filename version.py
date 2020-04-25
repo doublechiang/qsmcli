f0ca3ea76ab9537e5746985996d175a7396bdbb6
Jiang Junyu <double.chiang@gmail.com>
2020-04-24 21:22:33 -0700

    increase vesion to 0.5

--- version.py
+++ version.py
@@ -9,7 +9,7 @@ class Version(subcmd.SubCmd):
     Show version and credit information.
     Bug repoort and Suggestion please visit https://github.com/doublechiang/qsmcli
     """
-    VERSION='0.4'
+    VERSION='0.5'
 
     def printVersion(self, arg):
         print(self)
