149af130dd28650292ce53f93dd355a9e7ad68c7
Jiang Junyu <double.chiang@gmail.com>
2020-07-07 16:23:07 -0700

    complete package.sh

--- package.sh
+++ package.sh
@@ -3,7 +3,10 @@ set -e
 ./test.sh
 branch=`git rev-parse --abbrev-ref HEAD`
 if [[ $branch != 'master' ]]; then
-    read -p "git branch is not master, hit ctrl-c to break"
+    read -p "git branch is not master, press Y if you really want to continue.." input
+    if [[ $input != 'Y' ]]; then
+        exit 1
+    fi
 fi
 python3 setup.py sdist bdist_wheel
 echo "Generate packaged in dist/ folder...."
