41372e6deb274ad567df14fd2f08c545fd5b14e8
Jiang Junyu <double.chiang@gmail.com>
2020-12-28 16:15:50 -0800

    Change version string

--- src/version.py
+++ src/version.py
@@ -9,7 +9,7 @@ class Version(subcmd.SubCmd):
     Show version and credit information.
     Bug repoort and Suggestion please visit https://github.com/doublechiang/qsmcli
     """
-    VERSION='0.7.0'
+    VERSION='0.7.1'
 
     def printVersion(self, arg):
         print(self)
